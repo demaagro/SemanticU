24bf34ecd733963dcf6f46e90522b540786d4e8b
Dema <demaagro@hotmail.com>
2025-10-27 02:46:14 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/semanticui/main.go
@@ -0,0 +1,23 @@
+// cmd/semanticui/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "semanticui/internal/semanticui"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := semanticui.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
